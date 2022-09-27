61d90e94fd29c8e29bcd4ab6edbddc31531769ad
Jack Herdejurgen <Jack.D.Herdejurgen@ou.edu>
2022-09-27 09:40:06 -0500

    Add project files.

---
+++ MIS3033_004_EX2/Program.cs
@@ -0,0 +1,106 @@
+﻿// MIS 3033 004 Exercise
+// Jack Herdejurgen 113436899
+
+// add xinglong.ju@ou.edu as the collaborator on GitHub
+// 
+
+//****************************************************
+//Options Menu
+//1. Add a new student's information
+//2.Show all the students' information
+//3. Show the student's information with smallest age
+//4. Show the average weight of all the students
+//Press other keys to exit.
+//****************************************************
+
+//Enter your option: 
+
+//Add a new student
+//Enter the ID: 
+//Enter the name: 
+//Enter the age: 
+//Enter the weight:
+using MIS3033_004_EX2;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+string userinput = "";
+do
+{
+    Menu();
+    if (userinput == "1")
+    {
+        Option1();
+    }
+    else if (userinput == "2")
+    {
+        Option2();
+    }
+    else if (userinput == "3")
+    {
+        Option3();
+    }
+    else if (userinput == "4")
+    {
+        Option4();
+    }
+} 
+while (userinput == "1" || userinput == "2" || userinput == "3" || userinput == "4");
+
+
+void Menu()
+{
+    Console.WriteLine("1. Add new student's information");
+    Console.WriteLine("2. Show all students' information");
+    Console.WriteLine("3. Show the student's information with the smallest age");
+    Console.WriteLine("4. Show the average weight of all the students");
+    Console.WriteLine("Press other keys to exit.");
+    userinput = Console.ReadLine();
+}
+void Option1()
+{
+    Console.WriteLine("Add the new student:");
+    Console.Write("ID: ");
+    string ID = Console.ReadLine();
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Age: ");
+    int age = Convert.ToInt16(Console.ReadLine());
+    Console.Write("Weight: ");
+    double weight = Convert.ToDouble(Console.ReadLine());
+    Student stu = new Student(ID, name, age, weight);
+    Student.stulist.Add(stu);
+}
+void Option2()
+{
+    foreach(Student stu in Student.stulist)
+    {
+        Console.WriteLine(stu);
+    }
+}
+void Option3()
+{
+    Student lowestage = Student.stulist[0];
+    int minage = Student.stulist[0].age;
+    foreach(Student stu in Student.stulist)
+    {
+        if (stu.age < minage)
+        {
+            minage = stu.age;
+            lowestage = stu;
+        }
+    }
+    Console.WriteLine(lowestage);
+}
+void Option4()
+{
+    double sum = 0;
+    int count = 0;
+    foreach(Student stu in Student.stulist)
+    {
+        sum += stu.weight;
+        count++;
+    }
+    double average = sum / count;
+    Console.WriteLine($"Average weight: {average}");
+}
---
+++ MIS3033_004_EX2/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIS3033_004_EX2
+{
+    public class Student
+    {
+        public string ID { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public double weight { get; set; }
+        public static List<Student> stulist = new List<Student>();
+        public Student()
+        {
+
+        }
+        public Student(string ID, string name, int age, double weight)
+        {
+            this.ID = ID;
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+        }
+        public int AgeLevel()
+        {
+            if (this.age < 20)
+            {
+                return 1;
+            }
+            else if(age>=20 && age < 35)
+            {
+                return 2;
+            }
+            else if (age >= 35)
+            {
+                return 3;
+            }
+            else { return 0; }            
+        }
+        public override string ToString()
+        {
+            string s = $"{this.name} ({this.ID}) Age {this.age} (Level {this.AgeLevel()}), Weight: {this.weight} lbs";
+            return s;
+        }        
+    }
+}
